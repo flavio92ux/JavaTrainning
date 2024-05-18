31af9a8f9600168bd2616f8bd771547da3c95564
Flavio F <flavio.win@gmail.com>
2024-05-18 15:32:18 -0300

    Finalizando questao 15

--- exer15/Tabuleiro.java
+++ exer15/Tabuleiro.java
@@ -1,16 +1,30 @@
 package exer15;
 
+import java.util.Scanner;
+
 public class Tabuleiro {
   public Tabuleiro() {
-    for (int i = 1; i <= 6; i++) {
-      if (i == 6) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("TABULEIRO");
+    System.out.print("Dimensão do tabuleiro: ");
+    
+    int n = scanner.nextInt();
+
+    printarTabuleiro(n);
+
+    scanner.close();
+  }
+
+  private void printarTabuleiro(int n) {
+    for (int i = 1; i <= n; i++) {
+      if (i == n) {
         System.out.println("  " + i);
       } else {
         System.out.print("  " + i + " ");
       }
     }
 
-    for (int j = 1; j <= 6; j++) {
+    for (int j = 1; j <= n; j++) {
       String caracteres = "";
       boolean isCerquilha = true;
 
@@ -30,6 +44,5 @@ public class Tabuleiro {
 
       System.out.print(j + caracteres + "\n");
     }
-
   }
 }

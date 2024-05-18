2a44f7be5477a9d086a256db1b4dda970d5d7551
Flavio F <flavio.win@gmail.com>
2024-05-18 14:03:43 -0300

    Removendo pacotes nao necessarios

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-import exerc2.HazenWilliams;
-
 public class Main {
     public static void main(String[] args) {
         new HazenWilliams();
--- exerc2/HazenWilliams.java
+++ exerc2/HazenWilliams.java
@@ -1,5 +1,3 @@
-package exerc2;
-
 import java.util.Scanner;
 
 public class HazenWilliams {
